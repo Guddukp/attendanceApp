2ada785d6a9b7358c0798af7f234cf8db0e40ee4
Guddukp <40671918+Guddukp@users.noreply.github.com>
2019-03-16 19:25:51 +0530

    Init Commit
    The App so far

---
+++ MyApplication2/app/src/main/java/com/example/guddupatel/myapplication2/Activity/FirebaseAuth.java
@@ -0,0 +1,4 @@
+package com.example.guddupatel.myapplication2.Activity;
+
+class FirebaseAuth {
+}
---
+++ MyApplication2/app/src/main/java/com/example/guddupatel/myapplication2/TitlesActivity.java
@@ -0,0 +1,6 @@
+package com.example.guddupatel.myapplication2;
+
+import android.app.Activity;
+
+public class TitlesActivity extends Activity {
+}
