dd617fc68d1d3ad926b8d2afe626d61a016b0b52
Rik <rikr496@gmail.com>
2019-03-22 17:56:49 +0530

    Design Finalized

--- app/src/main/java/attendance/guddukp/com/attendance/MainActivity.java
+++ app/src/main/java/attendance/guddukp/com/attendance/MainActivity.java
@@ -18,12 +18,11 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  {
-private EditText et_pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et_pass=findViewById(R.id.et_pass);
         bind();
     }
 
@@ -32,12 +31,14 @@ private EditText et_pass;
         findViewById(R.id.bt_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Log In Recieved", Snackbar.LENGTH_INDEFINITE).setAction("OKAY", new View.OnClickListener() {
+                /*Snackbar.make(v, "Log In Recieved", Snackbar.LENGTH_INDEFINITE).setAction("OKAY", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
                     }
-                }).show();
+                }).show();*/
+                Intent intent = new Intent(MainActivity.this,Register_Activity.class);
+                startActivity(intent);
             }
         });
 
@@ -55,30 +56,24 @@ private EditText et_pass;
             }
         });
 
-        //Register Button
-        findViewById(R.id.bt_register).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "Sign Up Recieved", Snackbar.LENGTH_INDEFINITE).setAction("OKAY", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                }).show();
-            }
-        });
-
         //Visibility Toggle
-        findViewById(R.id.ib_show_pass).setOnTouchListener(new View.OnTouchListener() {
+        showHidePass(R.id.et_pass,R.id.ib_show_pass);
+    }
+
+    public void showHidePass(int p,int i){
+
+        final EditText pass = findViewById(p);
+
+        findViewById(i).setOnTouchListener(new View.OnTouchListener() {
+
                @Override
                public boolean onTouch(View v, MotionEvent event) {
-
                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                       et_pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                       pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                        v.setBackgroundResource(R.drawable.baseline_visibility_off_black_18dp);
                        return true;
                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                       et_pass.setInputType(InputType.TYPE_CLASS_TEXT |
+                       pass.setInputType(InputType.TYPE_CLASS_TEXT |
                                InputType.TYPE_TEXT_VARIATION_PASSWORD);
                        v.setBackgroundResource(R.drawable.baseline_visibility_black_18dp);
                        return true;
--- app/src/main/java/attendance/guddukp/com/attendance/Register_Activity.java
+++ app/src/main/java/attendance/guddukp/com/attendance/Register_Activity.java
@@ -10,71 +10,50 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Register_Activity extends AppCompatActivity {
-     private EditText rt_pass;
-     private EditText confirm_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
-        rt_pass=(EditText)findViewById(R.id.rt_pass);
-        confirm_pass=(EditText)findViewById(R.id.Confirm_Pass);
         bindActivity();
     }
 
     private void bindActivity() {
         //Register button
-        findViewById(R.id.rt_register).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt_register_r).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Register_Activity.this, "Sign Up Recived", Toast.LENGTH_SHORT).show();
             }
         });
-        //log In Button.
-        findViewById(R.id.rt_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Register_Activity.this, "Move on Log In page", Toast.LENGTH_SHORT).show();
-            }
-        });
-        //Visibility Toggle
-        findViewById(R.id.rb_show_pass).setOnTouchListener(new View.OnTouchListener() {
-                                                               @Override
-                                                               public boolean onTouch(View v, MotionEvent event) {
-                                                                   if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                                                       rt_pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                                                                       v.setBackgroundResource(R.drawable.baseline_visibility_off_black_18dp);
-                                                                       return true;
-                                                                   } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                                                                       rt_pass.setInputType(InputType.TYPE_CLASS_TEXT |
-                                                                               InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                                                                       v.setBackgroundResource(R.drawable.baseline_visibility_black_18dp);
-                                                                       return true;
-                                                                   }
 
-                                                                   return false;
-                                                               }
-                                                           }
-        );
         //Visibility Toggle Confirm pass
-        findViewById(R.id.rb_show_confirm_pass).setOnTouchListener(new View.OnTouchListener() {
-                                                               @Override
-                                                               public boolean onTouch(View v, MotionEvent event) {
+        showHidePass(R.id.et_pass_r, R.id.ib_show_pass_r);
+        showHidePass(R.id.et_rePass_r, R.id.ib_show_confirm_pass_r);
+    }
 
-                                                                   if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                                                       confirm_pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                                                                       v.setBackgroundResource(R.drawable.baseline_visibility_off_black_18dp);
-                                                                       return true;
-                                                                   } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                                                                       confirm_pass.setInputType(InputType.TYPE_CLASS_TEXT |
-                                                                               InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                                                                       v.setBackgroundResource(R.drawable.baseline_visibility_black_18dp);
-                                                                       return true;
-                                                                   }
+    public void showHidePass(int p,int i){
 
-                                                                   return false;
-                                                               }
-                                                           }
+        final EditText pass = findViewById(p);
+
+        findViewById(i).setOnTouchListener(new View.OnTouchListener() {
+
+               @Override
+               public boolean onTouch(View v, MotionEvent event) {
+                   if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                       pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                       v.setBackgroundResource(R.drawable.baseline_visibility_off_black_18dp);
+                       return true;
+                   } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                       pass.setInputType(InputType.TYPE_CLASS_TEXT |
+                               InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                       v.setBackgroundResource(R.drawable.baseline_visibility_black_18dp);
+                       return true;
+                   }
+
+                   return false;
+               }
+           }
         );
     }
 }
