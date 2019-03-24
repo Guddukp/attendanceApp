51094876445e418fa78897c5c040aa412f6ccc6d
Guddukp <40671918+Guddukp@users.noreply.github.com>
2019-03-24 14:59:45 +0530

    fragment
    ..

---
+++ app/src/main/java/attendance/guddukp/com/attendance/AdminActivity.java
@@ -0,0 +1,99 @@
+package attendance.guddukp.com.attendance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import fragment.StudentFragment;
+import fragment.TeacherFragment;
+
+public class AdminActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+   public LinearLayout fragmentArea;
+   private Fragment fragment;
+   protected FragmentManager manager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        bind();
+    }
+    private void bind(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+         fragmentArea=findViewById(R.id.fragment_area);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.admin, menu);
+        return true;
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_student:
+            // Handle the camera action
+                Intent intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+            break;
+            case R.id.nav_teacher:
+                Intent intent1=new Intent(this,RegisterActivity.class);
+                startActivity(intent1);
+             break;
+            case R.id.nav_attendance:
+                Toast.makeText(this, "Attendance", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_course:
+                Toast.makeText(this, "Course", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
